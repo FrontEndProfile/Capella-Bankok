4654ec906aa98529fe2f4d380192e6fd029fb4db
Fazal <102220108+FrontEndProfile@users.noreply.github.com>
2022-11-07 17:29:56 +0500

    v-02-js

--- index.js
+++ index.js
@@ -59,15 +59,45 @@ function waitForElm(selector) {
         '<div class="meth-link new"><img alt="visa" src="https://fazal-cdn.pages.dev/payment_03.png" width="36"></div>');
       $('.products-container .products-holder-main .products-holder').append('<i aria-hidden="true" class="product-column fake_div"></i><i aria-hidden="true" class="product-column fake_div"></i>');
 
+
+
+
       setTimeout(function () {
         $('.tab-content.expanded .row .col-lg-10').addClass('col-lg-12').removeClass('col-lg-10');
-        $(".variant-container").css("display", "none");
-        $("#personalise-form-id ").prepend(' <select class="form-control select-package"><option>Default select</option><option>select 02</option><option>select 03</option></select>');
+        // $(".variant-container").css("display", "none");
+        // $("#personalise-form-id ").prepend(' <select class="form-control select-package"><option>Default select</option><option>select 02</option><option>select 03</option></select>');
+
+        $(".custom-container.cart-container .cart-login-cont .login-cart-panel").prepend(' <p class="panel-note-text">Please note this login is for Capella Bangkok gifting platform only.</p>');
         $("#add-to-basket-btn").text("Buy Now");
         $(".related-products .custom-container").prepend(' <h3 class="section-title">More Like This</h3>');
         $('.related-products .container.custom-container .container').addClass('product-detail-container');
+
+        $(".cart-login-cont .guest-form-cont ").prepend(' <p class="panel-after-checkout">You can create account after checkout</p>');
+
+        $('.vendor-items-holder .vendor-items .ng-star-inserted .item-image-cont').addClass('col').removeClass('col-2');
+        $('.custom-container.cart-container.all').addClass('w-xxl-100').removeClass('container');
+        $('.custom-container.cart-container .cart-item.new .item-details .col-8.col-lg-7.pl-0').addClass('col-lg-6').removeClass('col-lg-7');
+
+        $('.custom-container.cart-container .item-details-cont .item-details .col-8.pl-0.col-lg-6').addClass('col px-0 product_item').removeClass('col-lg-6 pl-0 col-8');
+        $('.custom-container.cart-container .item-details-cont .item-details .col-lg-2.d-none.d-lg-block.text-center').addClass('col quantity_item').removeClass('col-lg-2 text-center');
+        $('.custom-container.cart-container .item-details-cont .item-details .col-4.col-lg-3.pr-0').addClass('col subtotal_item').removeClass('col-4 col-lg-3');
+        $('.custom-container.cart-container .vendor-items-holder .cart-item .col-10.px-0.item-details-cont').addClass('col').removeClass('col-10');
+
+        $('.custom-container.cart-container.w-xxl-100 .row .col-12.col-md-8.cart-left-cont').addClass('col-lg-8').removeClass('col-md-8');
+        $('.custom-container.cart-container.w-xxl-100 .row .col-12.col-md-4.cart-right-cont.ng-star-inserted').addClass('col-lg-4 col-10').removeClass('col-md-4 col-12');
+        $( ".custom-container.checkout .order-totals-container .form-group " ).addClass("d-block").removeClass("d-none");
+        $( ".custom-container.checkout .order-totals-container .form-group.pt-4.mb-0.d-none" ).addClass("d-block").removeClass("d-none");
+
+        $( "app-footer-checkout .container.custom-container.checkout-footer.text-center" ).empty().removeClass("container custom-container text-center").addClass("text-start");
+        $( "app-footer-checkout .checkout-footer.text-start" ).append(' <div class="ts-copyright"><span><a href="https://techsembly.com/" target="_blank">Techsembly©</a></span><span class="ml-1">2022</span></div> ');
+        
+
+        $('.edit-item-new img , .edit-contact img , .edit-address img').attr('src', 'https://static.techsembly.com/NPYRdytipVjtSV2vKtzLvrV6');
+
       }, 2000);
 
     });
   });
 })();
+
+
